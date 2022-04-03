b129ea412171800283b8942e115c9a27acc2c514
guadacastro <guadycasmar123@gmail.com>
2022-04-03 23:21:35 +0200

    ADD: detail page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,34 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
+import Detail from './components/Detail';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+
+      <Router>
+        <Header />
+        <Switch>
+
+          <Route path="/detail">
+            <Detail />
+          </Route>
+
+          <Route path="/">
+            <Home />
+          </Route>
+
+        </Switch>
+
+      </Router>  
+
     </div>
     
   );
---
+++ src/components/Movies.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import styled from 'styled-components'
+function Movies() {
+  return (
+    <Container>
+        <h4>Recomended for You</h4>
+        <Content>
+            <Wrap>
+                <img src="/images/simpsons.jpeg" />
+            </Wrap>
+            <Wrap>
+                <img src="/images/simpsons.jpeg" />
+            </Wrap>
+            <Wrap>
+                <img src="/images/simpsons.jpeg" />
+            </Wrap>
+            <Wrap>
+                <img src="/images/simpsons.jpeg" />
+            </Wrap>
+            <Wrap>
+                <img src="/images/simpsons.jpeg" />
+            </Wrap>
+            <Wrap>
+                <img src="/images/simpsons.jpeg" />
+            </Wrap>
+            <Wrap>
+                <img src="/images/simpsons.jpeg" />
+            </Wrap>
+            <Wrap>
+                <img src="/images/simpsons.jpeg" />
+            </Wrap>
+            <Wrap>
+                <img src="/images/simpsons.jpeg" />
+            </Wrap>
+        </Content>
+    </Container>
+  )
+}
+
+export default Movies
+
+const Container = styled.div`
+    
+`
+
+const Content = styled.div`
+    display: grid;
+    grid-gap: 25px;
+    grid-template-columns: repeat(5, minmax(0, 1fr));
+`
+
+const Wrap = styled.div`
+    border-radius: 10px;
+    cursor: pointer;
+    overflow: hidden;
+    border: 3px solid rgba(249, 249, 249, 0.1);
+    box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+    rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+    transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+
+    &:hover {
+        transform: scale(1.05);
+        box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
+        rgb(0 0 0 / 72%) 0px 30px 22px -10px;
+        border-color: rgba(249, 249, 249, 0.8);
+
+    }
+
+`
